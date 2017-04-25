1c769bc6e4dc604adb0375deae806b97f315ac46
patrick <patrickschultz@utexas.edu>
2017-04-25 15:34:59 -0500

    were stuck with the freaking match stmt

--- usname.scala
+++ usname.scala
@@ -9,21 +9,25 @@ def checker(new_name:String, old_names:List[String]) : String=
 	{
 	val sortednames:List[String] = old_names.sorted
 	println(new_name + "input list is length " + sortednames.length)
-	//set vars
+	//set vars, initialize
 	var found:Boolean = false
 	var startpoint:Int = 0
-	var endpoint:Int = sortednames.length/2
-	println("start: "+startpoint+" send: "+endpoint)
+	var endpoint:Int = sortednames.length-1
 	while (found == false)
 	{
-	var listwindow = sortednames.slice(startpoint, endpoint)
+	println("start: "+startpoint+" end: "+endpoint)
+	var listwindow : List[String] = sortednames.slice(0,3)
 	println(listwindow)
-	
-	//println(found)
-	} 
+	var mid :Int = startpoint + endpoint/2
+	new_name match
+	{
+		//case listwindow(2) => println("found")
+		case _ => "sth else"
+	}
+	}
 	"assikind"
 	}
 	
-val x:String =checker("ali", lines)
+val x:String =checker("assi", lines)
 
 println("checker returns "+x)
