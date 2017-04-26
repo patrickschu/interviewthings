b127fdf8634e536c7fd40969a53bbf3ee06d9b97
patrick <patrickschultz@utexas.edu>
2017-04-26 13:20:54 -0500

    fix the pattern matching

--- usname.scala
+++ usname.scala
@@ -2,7 +2,11 @@
 
 val lines = scala.io.Source.fromFile("/Users/ps22344/Downloads/interviewthings/namess.txt").getLines.toList
 
-
+def comparer(name_one:String, name_two:String): Boolean = name_one match 
+	{
+	case `name_two` => true
+	case _ => false
+	}
 
 println(lines)
 def checker(new_name:String, old_names:List[String]) : String=
@@ -13,17 +17,29 @@ def checker(new_name:String, old_names:List[String]) : String=
 	var found:Boolean = false
 	var startpoint:Int = 0
 	var endpoint:Int = sortednames.length-1
+	
 	while (found == false)
 	{
-	println("start: "+startpoint+" end: "+endpoint)
-	var listwindow : List[String] = sortednames.slice(0,3)
-	println(listwindow)
+	//println("start: "+startpoint+" end: "+endpoint)
+	var listwindow : List[String] = sortednames.slice(startpoint, endpoint)
+	//println(listwindow(2))
 	var mid :Int = startpoint + endpoint/2
-	new_name match
-	{
-		//case listwindow(2) => println("found")
-		case _ => "sth else"
-	}
+	println("mid is" + mid)
+	//val t = comparer("assi", "assi")
+	
+	found = comparer (new_name, listwindow(mid))	
+	
+	println("new name" + new_name)
+	println("istwindow" + listwindow(mid))
+	if (new_name > listwindow(mid)) {
+		startpoint = mid +1
+		}
+	if (new_name < listwindow(mid)) {
+		//not sure if this # is correct
+		endpoint = mid - 1 
+		}
+
+	println(found)
 	}
 	"assikind"
 	}
